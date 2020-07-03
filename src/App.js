412243c8c116518738ec43f62fd8121fb0ea8524
Fernando Arias <fernandorafaelarias@gmail.com>
2020-07-02 21:54:26 -0300

    3.14 Funciones y elementos

--- src/App.js
+++ src/App.js
@@ -9,15 +9,19 @@ function Hello ( props ) {
 class Text extends Component {
   render() {
 
+    const { arrayOfNumbers, multiply, objectWithInfo, title } = this.props;
+
     const textoSegunBool = this.props.boolean ? 'Es cierto!' : 'Falso';
+    const mappedNumbers = arrayOfNumbers.map(multiply);
 
     return (
       <div>
+        {title}
         <p>{this.props.text}</p>
         <p>{this.props.number}</p>
         <p>{textoSegunBool}</p>
-        <p>{ this.props.arrayOfNumbers.join(', ') }</p>
-        <p>{ this.props.objectWithInfo.key }</p>
+        <p>{ mappedNumbers.join(', ') }</p>
+        <p>{ objectWithInfo.key }</p>
       </div>
     )
   }
@@ -34,8 +38,10 @@ function App() {
         arrayOfNumbers={[2,3,10]}
         objectWithInfo={{ key: 'value', key2: 'otherValue' }}
         text="Texto en string"
+        multiply={ (number) => number * 4 }
         number={2}
-        boolean={false} />
+        boolean={false}
+        title={<h1>Este es el título</h1>} />
     </div>
   );
 }
